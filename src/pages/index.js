97ed4ba9fba8597ae712c5278ab7a868ba3139c8
Touko Vainio-Kaila <touko.vk@gmail.com>
2020-05-22 17:26:52 +0300

    Render books in ebooks.csv (POC)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,51 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const Ebook = ebook => (
+  <div style={{width: '100%', display: `inline-block`, margin: `0 1rem 1rem 0` }}>
+    <div style={{display: 'block', width: '100%'}}>
+      <a href={ebook.link} style={{fontSize: '1.2rem'}}>
+      {ebook.title}
+      </a>
+      <span className='date'> ({ebook.author}, {ebook.year})</span>
+    </div>
+    <span className='media'>{ebook.publisher}</span>
+  </div>
+)
+const IndexPage = ({ data }) => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <SEO title="Ebooks" />
+    <h1>Ebooks</h1>
+    {data.allEbooksCsv.edges.map(( {node} ) => (
+      <Ebook {...node} />
+    ))}
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    allEbooksCsv {
+      edges {
+        node {
+          id
+          title
+          author
+          publisher
+          year
+          isbn
+          link
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
