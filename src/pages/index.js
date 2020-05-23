1dbd718d484617eede9fc176cba2ee62b8a35493
Touko Vainio-Kaila <touko.vk@gmail.com>
2020-05-23 15:38:32 +0300

    Add sorting

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,6 +20,52 @@ function filterBooks(books, search) {
   })
 }
 
+function clean(s) {
+  return s ? s.toLowerCase() : ''
+}
+
+const sortGetters = {
+  'title': (book) => clean(book.title),
+  'author': (book) => clean(book.author),
+  'publisher': (book) => clean(book.publisher),
+  'year': (book) => parseInt(book.year),
+}
+
+function sortBooks(books, sort) {
+  if (!sort) {
+    console.log('Unexpected situation: sort parameter missing', sort)
+    return books
+  }
+  let getter
+  let factor
+  if (sort.endsWith('-asc')) {
+    factor = 1
+    getter = sortGetters[sort.substring(0, sort.length - 4)]
+  } else if(sort.endsWith('-desc')) {
+    factor = -1
+    getter = sortGetters[sort.substring(0, sort.length - 5)]
+  } else {
+    console.log('Unexpected sort:', sort)
+    return books
+  }
+  if (!getter || !factor) {
+    console.log('Unexpected situation: Missing getter/factor:', getter, factor)
+    return books
+  }
+  function compare(book1, book2) {
+    const value1 = getter(book1)
+    const value2 = getter(book2)
+    if (value1 > value2) {
+      return factor
+    } else if (value1 < value2) {
+      return -factor
+    } else {
+      return 0
+    }
+  }
+  return books.concat().sort(compare)
+}
+
 const Ebook = ebook => (
   <div className='book'>
     <div className='title-link'>
@@ -30,18 +76,32 @@ const Ebook = ebook => (
 )
 
 const IndexPage = ({ data }) => {
-  const [ state, setState ] = useState({search: ''});
+  const [ state, setState ] = useState({search: '', sort: 'title-asc'});
   const onKeyDown = function(event) {
     if (event.key === 'Enter') {
       setState({...state, search: event.target.value})
     }
   }
   const books = filterBooks(data.allEbooksCsv.edges.map(edge => edge.node), state.search)
+  const sortedBooks = sortBooks(books, state.sort)
   return (
     <Layout>
       <input className='search' type='text' onKeyDown={onKeyDown} placeholder='Text to sarch'/>
+      <div className='sort'>
+        <span>Sort by:</span>
+        <select value={state.sort} onChange={(event) => setState({...state, sort: event.target.value})}>
+        <option value="title-asc">Title (asc.)</option>
+          <option value="title-desc">Title (desc.)</option>
+          <option value="author-asc">Author (asc.)</option>
+          <option value="author-desc">Author (desc.)</option>
+          <option value="publisher-asc">Publisher (asc.)</option>
+          <option value="publisher-desc">Publisher (desc.)</option>
+          <option value="year-asc">Year (asc.)</option>
+          <option value="year-desc">Year (desc.)</option>
+        </select>
+      </div>
       <SEO title="My Ebooks" />
-      {books.map((node) => (
+      {sortedBooks.map((node) => (
         <Ebook {...node} key={node.id} />
       ))}
     </Layout>
