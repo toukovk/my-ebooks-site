006062f8a3253680553d498137a132a127a41547
Touko Vainio-Kaila <touko.vk@gmail.com>
2020-05-23 15:05:01 +0300

    Add search/filtering

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,24 @@
-import React from "react"
+import React, { useState } from "react"
 import { graphql} from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+
+function filterBooks(books, search) {
+  if (!search) {
+    return books
+  }
+  const searchLowercase = search.trim().toLowerCase()
+  const containsSearch = (s) => s && s.toLowerCase().indexOf(searchLowercase) !== -1
+  return books.filter((book) => {
+    return (
+      containsSearch(book.title) ||
+      containsSearch(book.author) ||
+      containsSearch(book.publisher) ||
+      containsSearch(book.year)
+    )
+  })
+}
 
 const Ebook = ebook => (
   <div className='book'>
@@ -12,14 +28,25 @@ const Ebook = ebook => (
     {ebook.author} - {ebook.publisher} ({ebook.year})
   </div>
 )
-const IndexPage = ({ data }) => (
-  <Layout>
-    <SEO title="My Ebooks" />
-    {data.allEbooksCsv.edges.map(( {node} ) => (
-      <Ebook {...node} />
-    ))}
-  </Layout>
-)
+
+const IndexPage = ({ data }) => {
+  const [ state, setState ] = useState({search: ''});
+  const onKeyDown = function(event) {
+    if (event.key === 'Enter') {
+      setState({...state, search: event.target.value})
+    }
+  }
+  const books = filterBooks(data.allEbooksCsv.edges.map(edge => edge.node), state.search)
+  return (
+    <Layout>
+      <input className='search' type='text' onKeyDown={onKeyDown} placeholder='Text to sarch'/>
+      <SEO title="My Ebooks" />
+      {books.map((node) => (
+        <Ebook {...node} key={node.id} />
+      ))}
+    </Layout>
+  )
+}
 
 export const query = graphql`
   query {
